37535a00a12da0b2d4fdf0071cfe7294fb348e61
jsprodev <jsprodev@gmail.com>
2025-09-04 02:55:55 +0500

    footer design fix

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -15,13 +15,12 @@ export const Footer = () => {
     <footer className="bg-slate-900 text-slate-400 py-12 text-sm tracking-wide">
       <div className="max-w-7xl mx-auto px-6 lg:px-8">
         <div className="flex flex-col items-center md:flex-row flex-wrap justify-between">
-          <div className="max-md:mb-6 basis-[50%] pr-5 space-y-3">
+          <div className="flex flex-col w-1/2 max-md:w-full max-md:items-center gap-y-3">
             <Logo className="dark" />
-            <p className="max-md:text-center">
-              Front End Engineer with a solid foundation in web development principles and a passion for creating
-              visually appealing and functional user interfaces.
-            </p>
-            <div className="flex space-x-4 items-center mt-4">
+            <div className="max-md:text-center m-0">
+              Crafting modern, human-centered interfaces that deliver seamless user experiences.
+            </div>
+            <div className="flex space-x-4 items-center mt-2">
               <a href="https://www.linkedin.com/in/hashimkhalid/" target="_blank" rel="noopener noreferrer">
                 <Linkedin size={18} />
                 <span className="sr-only">Linkedin</span>
@@ -37,69 +36,73 @@ export const Footer = () => {
             </div>
           </div>
 
-          <div className="flex flex-col items-center md:items-end basis-[50%]">
-            <br />
-            <div className="flex items-center">
-              © 2025 HBK | Built with &nbsp;
-              <Tooltip>
-                <TooltipTrigger>
-                  <div className="bg-slate-700 p-1 rounded-sm">
-                    <React className="h-4 w-4" />
-                  </div>
-                </TooltipTrigger>
-                <TooltipContent
-                  className="bg-slate-700 text-slate-300"
-                  arrowBackground={"bg-slate-700"}
-                  arrowFill={"fill-slate-700"}
-                >
-                  <p>React</p>
-                </TooltipContent>
-              </Tooltip>
-              <span className="w-5 text-center">+</span>
-              <Tooltip>
-                <TooltipTrigger>
-                  <div className="bg-slate-700 p-1 rounded-sm">
-                    <img src={vite} alt="Vite logo" className="h-4 w-4" />
-                  </div>
-                </TooltipTrigger>
-                <TooltipContent
-                  className="bg-slate-700 text-slate-300"
-                  arrowBackground={"bg-slate-700"}
-                  arrowFill={"fill-slate-700"}
-                >
-                  <p>Vite</p>
-                </TooltipContent>
-              </Tooltip>
-              <span className="w-5 text-center">+</span>
-              <Tooltip>
-                <TooltipTrigger>
-                  <div className="bg-slate-700 p-1 rounded-sm">
-                    <Tailwind className="h-4 w-4" />
-                  </div>
-                </TooltipTrigger>
-                <TooltipContent
-                  className="bg-slate-700 text-slate-300"
-                  arrowBackground={"bg-slate-700"}
-                  arrowFill={"fill-slate-700"}
-                >
-                  <p>TailwindCSS</p>
-                </TooltipContent>
-              </Tooltip>
-              <span className="w-5 text-center">+</span>
-              <Tooltip>
-                <TooltipTrigger>
-                  <div className="bg-slate-700 p-1 rounded-sm">
-                    <Shadcn className="h-4 w-4" />
-                  </div>
-                </TooltipTrigger>
-                <TooltipContent
-                  className="bg-slate-700 text-slate-300"
-                  arrowBackground={"bg-slate-700"}
-                  arrowFill={"fill-slate-700"}
-                >
-                  <p>Shadcn</p>
-                </TooltipContent>
-              </Tooltip>
+          <div className="flex flex-col items-center md:items-end max-md:width-full">
+            <div className="flex items-center max-md:flex-col gap-y-4 mt-5">
+              <div className="flex items-center">
+                © 2025 hbk &nbsp; <span className="max-md:hidden">|</span>
+              </div>
+              <div className="flex items-center">
+                &nbsp; Built with &nbsp;
+                <Tooltip>
+                  <TooltipTrigger>
+                    <div className="bg-slate-700 p-1 rounded-sm">
+                      <React className="h-4 w-4" />
+                    </div>
+                  </TooltipTrigger>
+                  <TooltipContent
+                    className="bg-slate-700 text-slate-300"
+                    arrowBackground={"bg-slate-700"}
+                    arrowFill={"fill-slate-700"}
+                  >
+                    <p>React</p>
+                  </TooltipContent>
+                </Tooltip>
+                <span className="w-5 text-center">+</span>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <div className="bg-slate-700 p-1 rounded-sm">
+                      <img src={vite} alt="Vite logo" className="h-4 w-4" />
+                    </div>
+                  </TooltipTrigger>
+                  <TooltipContent
+                    className="bg-slate-700 text-slate-300"
+                    arrowBackground={"bg-slate-700"}
+                    arrowFill={"fill-slate-700"}
+                  >
+                    <p>Vite</p>
+                  </TooltipContent>
+                </Tooltip>
+                <span className="w-5 text-center">+</span>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <div className="bg-slate-700 p-1 rounded-sm">
+                      <Tailwind className="h-4 w-4" />
+                    </div>
+                  </TooltipTrigger>
+                  <TooltipContent
+                    className="bg-slate-700 text-slate-300"
+                    arrowBackground={"bg-slate-700"}
+                    arrowFill={"fill-slate-700"}
+                  >
+                    <p>TailwindCSS</p>
+                  </TooltipContent>
+                </Tooltip>
+                <span className="w-5 text-center">+</span>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <div className="bg-slate-700 p-1 rounded-sm">
+                      <Shadcn className="h-4 w-4" />
+                    </div>
+                  </TooltipTrigger>
+                  <TooltipContent
+                    className="bg-slate-700 text-slate-300"
+                    arrowBackground={"bg-slate-700"}
+                    arrowFill={"fill-slate-700"}
+                  >
+                    <p>Shadcn</p>
+                  </TooltipContent>
+                </Tooltip>
+              </div>
             </div>
           </div>
         </div>
